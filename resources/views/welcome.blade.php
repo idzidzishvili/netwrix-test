f3a9f5164d420752345965b192fe6cce8ad7e6cd
Ilia Dzidzishvili <ilia.dzidzishvili@gmail.com>
2023-02-28 11:53:19 +0400

    axios

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,11 +11,31 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
         <!-- Styles -->
-        @vite(['resources/scss/app.scss', 'resources/js/app.js'])
+        {{-- @vite(['resources/scss/app.scss', 'resources/js/app.js']) --}}
+        <link rel="stylesheet" href="{{asset('build/assets/app-3b6ee705.css')}}">
+        {{-- https://volt-pro-laravel-admin-dashboard.updivision.com/documentation/plugins/choices-js/index.html --}}
+        {{-- npm run build --}}
     </head>
     <body class="antialiased">
        <div class="test">
         Hello
        </div>
+       <script>
+        const BASE_URL = 'https://jsonplaceholder.typicode.com';
+        const getTodoItems = async () => {
+            try {
+                const response = await axios.get(`${BASE_URL}/todos?_limit=5`);
+                console.log(response)
+
+            const todoItems = response.data;
+
+            console.log(`GET: Here's the list of todos`, todoItems);
+
+            return todoItems;
+        } catch (errors) {
+            console.error(errors);
+        }
+        };
+       </script>
     </body>
 </html>
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import pkg from './package.json';
+const { resolve } = require('path')
 
 export default defineConfig({
     plugins: [
